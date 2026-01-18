d8d3636d4bb3620baf4e6012031658d6c4570a09
Hamad | Applied Data Science (Python, SQL, ML) <codebyhamad@gmail.com>
2026-01-18 13:07:47 +0500

    Fixed paths and removed large file

---
+++ app.py
@@ -0,0 +1,42 @@
+import pickle
+import streamlit as st
+import requests
+
+def fetch_poster(movie_id):
+    url = f"https://api.themoviedb.org/3/movie/{movie_id}?api_key=8265bd1679663a7ea12ac168da84d2e8&language=en-US"
+    try:
+        data = requests.get(url).json()
+        poster_path = data.get('poster_path')
+        if poster_path:
+            return "https://image.tmdb.org/t/p/w500/" + poster_path.lstrip('/')
+        return "https://via.placeholder.com/500x750?text=No+Poster"
+    except:
+        return "https://via.placeholder.com/500x750?text=Error"
+
+def recommend(movie):
+    index = movies[movies['title'] == movie].index[0]
+    distances = sorted(list(enumerate(similarity[index])), reverse=True, key=lambda x: x[1])
+    names = []
+    posters = []
+    for i in distances[1:6]:
+        movie_id = movies.iloc[i[0]].movie_id
+        posters.append(fetch_poster(movie_id))
+        names.append(movies.iloc[i[0]].title)
+    return names, posters
+
+st.header('Movie Recommender System')
+
+# Use relative paths so it works on GitHub
+movies = pickle.load(open('movie_list.pkl','rb'))
+similarity = pickle.load(open('similarity.pkl','rb'))
+
+movie_list = movies['title'].values
+selected_movie = st.selectbox("Select a movie", movie_list)
+
+if st.button('Show Recommendation'):
+    names, posters = recommend(selected_movie)
+    cols = st.columns(5)
+    for i in range(5):
+        with cols[i]:
+            st.text(names[i])
+            st.image(posters[i])
